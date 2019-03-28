900765452cdd27b09787dd7433b30d7bcd319f1f
Kyle Jessup <kyle.jessup@treefrog.ca>
2019-03-28 17:58:16 -0400

    Added helper func

--- shim.h
+++ shim.h
@@ -1,4 +1,10 @@
 
 #include <mongoc.h>
 
-
+static bool _mongoc_cursor_next(mongoc_cursor_t *cursor, const void **bson)
+{
+	const bson_t *bson2 = NULL;
+	bool r = mongoc_cursor_next(cursor, &bson2);
+	*bson = bson2;
+	return r;
+}
